70208c78d09b22510ffe6e09905a689509d4dd76
Alexandre11021998 <alexandre.dev.costa@gmail.com>
2026-07-16 18:43:39 -0300

    ajustes layout

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // ⬅️ O segredo está aqui! Mudou para plugin-react
+import tailwindcss from "@tailwindcss/vite";
+import path from "path"; // Geralmente usado pelo Shadcn UI para os caminhos com "@"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
